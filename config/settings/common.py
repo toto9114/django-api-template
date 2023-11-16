c38ddf02e23f815d5949e0558e7d08e552825254
Allen <jihun3353@gmail.com>
2023-11-17 07:21:18 +0900

    update

--- config/settings/common.py
+++ config/settings/common.py
@@ -18,8 +18,7 @@ from config import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -36,7 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.root"
+    "apps.root",
 ]
 
 MIDDLEWARE = [
